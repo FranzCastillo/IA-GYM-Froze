69aa8e9d771b25326a7b97a4282b399f3909b874
bl33h <per21371@uvg.edu.gt>
2024-03-01 18:13:09 -0600

    random map per execution

--- src/reinforcementFrozenLake.py
+++ src/reinforcementFrozenLake.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 import gymnasium as gym
 import numpy as np
 import pickle
 
+desc=["SFFF", "FHFH", "FFFH", "HFFG"]
+ 
 def run(episodes, agentMode=True, render=False): # agentMode: true for training, false for evaluation
     # initialize the environment with given parameters
-    env = gym.make('FrozenLake-v1', map_name="4x4", is_slippery=True, render_mode='human' if render else None)
+    env = gym.make('FrozenLake-v1', desc=generate_random_map(size=4), is_slippery=True, render_mode='human' if render else None)
 
     # load or initialize q table
     if agentMode:
